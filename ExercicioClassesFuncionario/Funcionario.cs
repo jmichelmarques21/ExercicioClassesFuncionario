57a260776c702cdc4c82ee39d7614a1c7bf542e0
Jean Marques <jmichelmarques21@outlook.com>
2023-05-06 00:37:48 -0300

    .

---
+++ ExercicioClassesFuncionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassesFuncionario {
+    internal class Funcionario {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);            
+        }
+
+        override public string ToString() {
+            return Nome
+                + ", Salário líquido: R$"
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
--- ExercicioClassesFuncionario/Program.cs
+++ ExercicioClassesFuncionario/Program.cs
@@ -9,8 +9,29 @@ namespace ExercicioClassesFuncionario {
              * funcionário (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o
              * salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.*/
 
-            Console.Write("Nome: ");
+            Funcionario func = new Funcionario();
 
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porc);
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + func);
 
 
 
